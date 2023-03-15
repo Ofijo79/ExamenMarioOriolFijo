bf38a57904008792ec6bfb3b8553ec845248b164
Ofijo79 <oriolfijo2004@gmail.com>
2023-03-15 10:23:02 +0100

    Examen Comentado

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class GroundSensor : MonoBehaviour
 {
-    public bool isGrounded;
+    public bool isGrounded; // variable para que el GroundSensor detecte el suelo
 
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerEnter2D(Collider2D other) //funcion para que cuando entre al suelo lo detecte, no se vaya debajo del suelo y pueda saltar
     {
-        if(other.gameObject.layer == 3)
+        if(other.gameObject.layer == 3) // el layer es el que hemos creado dentro del unity que tiene el numero 3 podriamos ponerle tambien ("Ground")
         {
             isGrounded = true;
         }
     }
 
-    void OnTriggerStay2D(Collider2D other) 
+    void OnTriggerStay2D(Collider2D other) //funcion para que cuando este en el suelo lo detecte, no se vaya debajo del suelo y pueda saltar
     {
         if(other.gameObject.layer == 3)
         {
@@ -22,7 +22,7 @@ public class GroundSensor : MonoBehaviour
         }
     }
     
-    void OnTriggerExit2D(Collider2D other) 
+    void OnTriggerExit2D(Collider2D other) //funcion para que cuando no este en el suelo lo detecte y no te deje hacer saltos infinitos
     {
         if(other.gameObject.layer == 3)
         {
--- Assets/Scripts/MarioMovement.cs
+++ Assets/Scripts/MarioMovement.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class MarioMovement : MonoBehaviour
 {
-    public float jumpForce = 3f;
-    public float playerSpeed = 5.5f;
-    float horizontal;
+    public float jumpForce = 3f; // variable de la fuerza de salto
+    public float playerSpeed = 5.5f; // variable de la velocidad del personaje
+    float horizontal; // variable para que podamos movernos y girar el sprite
 
 
-    private GroundSensor sensor;
-    private Rigidbody2D rBody;
-    private SpriteRenderer spriteRenderer;
+    private GroundSensor sensor; // variable para que podamos detectar el script del ground sensor
+    private Rigidbody2D rBody; // variable para que podamos detectar el RigidBody2D
+    private SpriteRenderer spriteRenderer; // variable para que podamos girar el sprite
 
 
     // Start is called before the first frame update
     void Start()
     {
-        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>();
+        sensor = GameObject.Find("GroundSensor").GetComponent<GroundSensor>(); // funciones para encontrar el script y usar-lo
         rBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -25,7 +25,7 @@ public class MarioMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal"); // funcion para que se gire el sprite
 
         if(horizontal < 0)
         {
@@ -35,13 +35,13 @@ public class MarioMovement : MonoBehaviour
         {
             spriteRenderer.flipX = false;
         }
-        if(Input.GetButtonDown("Jump") && sensor.isGrounded) 
+        if(Input.GetButtonDown("Jump") && sensor.isGrounded)  // funcion para el salto
         {
             rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
     
-    void FixedUpdate() 
+    void FixedUpdate() // funcion para tener la velocidad del mario
     {
         rBody.velocity = new Vector2(horizontal * playerSpeed, rBody.velocity.y);    
     }
